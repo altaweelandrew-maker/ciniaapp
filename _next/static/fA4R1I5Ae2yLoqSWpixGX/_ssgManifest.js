2881c0cf5633f1e1fd423249a4fbde3ca35ffb6a
altaweelandrew-maker <altaweelandrew@users.noreply.github.com>
2026-09-13 12:17:09 +0400

    feat: add compiled static export to /docs and finalize GitHub Pages configuration

---
+++ _next/static/fA4R1I5Ae2yLoqSWpixGX/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fcheckout\u002F[bookingId]","\u002Fmovies\u002F[id]","\u002Fshowtimes\u002F[id]\u002Fseats","\u002Ftickets\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
